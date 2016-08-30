56ee78b0ff590b3de6b8f04c94416544a8b46f99
Robin <robin@dapplo.net>
2016-08-30 13:09:46 +0200

    Fixed missing coveralls [release]

--- src/CreateCoverage.ps1
+++ src/CreateCoverage.ps1
@@ -2,22 +2,22 @@
 # 1: using OpenCover to run the xunit tests
 # 2: ReportGenerator for the HTML report (if COVERALLS_REPO_TOKEN NOT available)
 # 3: Coveralls.net to export the generated OpenCover report to coveralls.io (if COVERALLS_REPO_TOKEN available)
-param(
-[string]$solutionDirectory
-)
 
+param(
+[Parameter(Mandatory=$true)][string]$solutionDirectory
+)
 
 $projectName = (gci $solutionDirectory\*.Sln).BaseName
 $filter="-filter:+`"[$projectName]* +[$projectName.Bootstrapper]*`""
 $opencoverPath = ((gci $solutionDirectory\packages\opencover* | sort-object name)[-1]).Fullname
 $xunitrunnerPath = ((gci $solutionDirectory\packages\xunit.runner.console* | sort-object name)[-1]).Fullname
+$coverallsPath = ((gci $solutionDirectory\packages\coveralls.io* | sort-object name)[-1]).Fullname
 $output="$solutionDirectory\coverage.xml"
 
 $openCoverArguments = @("-register:user", "$filter", "-target:$xunitrunnerPath\tools\xunit.console.exe", "-targetargs:`"$solutionDirectory\$projectName.Tests\bin\release\$projectName.Tests.dll -noshadow -xml xunit.xml`"","-output:`"$output`"")
 Start-Process -wait $opencoverPath\tools\OpenCover.Console.exe -NoNewWindow -ArgumentList $openCoverArguments
 
 if (Test-Path Env:COVERALLS_REPO_TOKEN) {
-	$coverallsPath = ((gci $solutionDirectory\packages\\coveralls.io* | sort-object name)[-1]).Fullname
 	$coverallsArguments = @("--opencover $output")
 	Start-Process -wait $coverallsPath\tools\coveralls.net.exe -NoNewWindow -ArgumentList $coverallsArguments
 }
