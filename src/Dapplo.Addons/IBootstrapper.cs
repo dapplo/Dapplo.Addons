83b0354bc1bff812bbbb723e2156227848f72838
Robin <robin@dapplo.net>
2016-10-29 00:54:15 +0200

    Fix for CancellationToken without default in interface [release]

--- src/Dapplo.Addons/IBootstrapper.cs
+++ src/Dapplo.Addons/IBootstrapper.cs
@@ -46,17 +46,17 @@ namespace Dapplo.Addons
 		/// <summary>
 		///     Initialize the bootstrapper
 		/// </summary>
-		Task<bool> InitializeAsync(CancellationToken cancellationToken);
+		Task<bool> InitializeAsync(CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		///     Start the bootstrapper, initialize is automatically called when needed
 		/// </summary>
-		Task<bool> RunAsync(CancellationToken cancellationToken);
+		Task<bool> RunAsync(CancellationToken cancellationToken = default(CancellationToken));
 
 		/// <summary>
 		///     Stop the bootstrapper, this cleans up resources and makes it possible to hook into it.
 		///     Is also called when being disposed, but as Dispose in not Async this could cause some issues.
 		/// </summary>
-		Task<bool> StopAsync(CancellationToken cancellationToken);
+		Task<bool> StopAsync(CancellationToken cancellationToken = default(CancellationToken));
 	}
 }
