1e4422de716f79e06fa331827e17585babffa999
Robin <robin@dapplo.net>
2017-09-25 09:17:42 +0200

    Enabled Coveralls.io again, as cake-contrib/Cake.Coveralls#26 is fixed.

--- build.ps1
+++ build.ps1
@@ -228,5 +228,5 @@ if (!(Test-Path $CAKE_EXE)) {
 
 # Start Cake
 Write-Host "Running build script..."
-Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" --settings_skipverification=true -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
+Invoke-Expression "& `"$CAKE_EXE`" `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental $ScriptArgs"
 exit $LASTEXITCODE
